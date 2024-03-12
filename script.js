56633017112cb797fecf6099652786b3e762ed82
TaHyc <stas.rogal89@gmail.com>
2024-03-12 23:07:48 +0300

    адаптив под мобильные устройства

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ slider.addEventListener("change", function() {
     console.log(slider.value)
   valueDisplay.textContent = `${slider.value} %` ;
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector("header").classList.toggle("open")
-    })
-})
