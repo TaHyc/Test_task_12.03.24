64c61a7c8f2cf6be45b0b07ea452f99ceda04901
TaHyc <stas.rogal89@gmail.com>
2024-03-13 01:40:21 +0300

    Animation

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const slider = document.getElementById("percentSlider");
 const valueDisplay = document.getElementById("sliderValue");
+let scrollTopButton = document.getElementById("scrollBtn");
 
 slider.addEventListener("change", function() {
     console.log(slider.value)
   valueDisplay.textContent = `${slider.value} %` ;
+});
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    scrollTopButton.classList.add("show");
+    scrollTopButton.addEventListener("click", function () {
+      window.scrollTo({top: 0,behavior: "smooth"});
+    })
+  } else {
+    scrollTopButton.classList.remove("show");
+  }
 });
